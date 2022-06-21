67907cbef7663b4942a18377985ff9b06bd9b959
ayobamy <ayobamyahmed@gmail.com>
2022-06-21 14:54:11 +0000

    added main file

---
+++ arithmetic-calculator/main.js
@@ -0,0 +1,9 @@
+// Basic Arithmetic Calculator
+// This calculator does:
+// 1. Addition (+)
+// 2. Subtraction (-)
+// 3. Multiplication (*)
+// 4. Division (/)
+// 5. Modulo Operations (%)
+
+const num1 = prompt("Enter your first number:  ")
