1df71d92573c08e11957b3f300046cc0dadd2e3e
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-06-23 13:58:30 +0000

    made changes

--- arithmetic-calculator/README.md
+++ arithmetic-calculator/README.md
@@ -3,3 +3,15 @@
 
 ## Link to the hosted page on GitHub
 https://ayobamy.github.io/zuri-javascript/arithmetic-calculator/
+
+
+## ALGORITHM
+### Steps:
+1. prompt user for operator
+2. store it in a variable
+3. prompt user to input the first number
+4. prompt user to input the second number
+4. store both input numbers in a varibale
+5. create a variable to store the result of the operation
+6. create conditional statements (if/else OR switch/break) to perform each operations
+7. Alert the result of the operation to the user
--- arithmetic-calculator/main.js
+++ arithmetic-calculator/main.js
@@ -6,15 +6,6 @@
 // 4. Division (/)
 // 5. Modulo Operations (%)
 
-// ALGORITHM
-// Step 1: prompt user for operator
-// Step 2: store it in a variable
-// Step 3: prompt user for first and second number
-// Step 4: store it in a varibale
-// Step 5: store the result of the operation
-// Step 6: create conditional statements to perform operations
-// Step 7: Alert the result of the operation to the user
-
 const op_list = prompt('Enter operator ( either +, -, *, / or % ): ');
 
 const num1 = parseFloat(prompt("Enter your first number: "));
